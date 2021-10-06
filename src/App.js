1c0fbd11211d77d76fa254680167e89effe1a891
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-06 15:03:12 +0300

    const [likes, setLikes] = useState(0); const Increment = () => {     setLikes(likes + 1);   };   <button onClick={Increment}>Increment</button>

--- src/App.js
+++ src/App.js
@@ -2,27 +2,20 @@ import React, { useState } from 'react';
 
 const App = () => {
   // const state = useState(0);
-  let [likes, setLikes] = useState(0);
-  console.log(likes);
+  const [likes, setLikes] = useState(0);
+
+  const Increment = () => {
+    setLikes(likes + 1);
+  };
+  const Decrement = () => {
+    setLikes(likes - 1);
+  };
+
   return (
     <div>
       <h1>{likes}</h1>
-      <button
-        onClick={() => {
-          setLikes((likes += 1));
-          console.log(likes);
-        }}
-      >
-        Increment
-      </button>
-      <button
-        onClick={() => {
-          setLikes((likes -= 1));
-          console.log(likes);
-        }}
-      >
-        Decrement
-      </button>
+      <button onClick={Increment}>Increment</button>
+      <button onClick={Decrement}>Decrement</button>
     </div>
   );
 };
