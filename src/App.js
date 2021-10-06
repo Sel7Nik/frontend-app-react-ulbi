c38dd6d0cc58b3e5ebcbab22d8c31a94af5e06f6
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-06 15:11:41 +0300

    const [value, setValue] = useState('ТЕКСТ'); <input type="text" value={value} onChange={onChange} />  <input type="text" value={value} onChange={onChange} />

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  // const state = useState(0);
   const [likes, setLikes] = useState(0);
+  const [value, setValue] = useState('ТЕКСТ');
 
   const Increment = () => {
     setLikes(likes + 1);
@@ -10,10 +10,16 @@ const App = () => {
   const Decrement = () => {
     setLikes(likes - 1);
   };
+  const onChange = (event) => {
+    setValue(event.target.value);
+    console.log(event.target.value);
+  };
 
   return (
     <div>
       <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input type="text" value={value} onChange={onChange} />
       <button onClick={Increment}>Increment</button>
       <button onClick={Decrement}>Decrement</button>
     </div>
