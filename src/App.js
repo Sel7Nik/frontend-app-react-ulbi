46ec43eb0a1f90dde209386b5d533a6ef3ef57dd
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-06 15:18:49 +0300

    Increment component Counter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Counter from './components/Counter';
 
 const App = () => {
-  const [likes, setLikes] = useState(0);
-  const [value, setValue] = useState('ТЕКСТ');
-
-  const Increment = () => {
-    setLikes(likes + 1);
-  };
-  const Decrement = () => {
-    setLikes(likes - 1);
-  };
-  const onChange = (event) => {
-    setValue(event.target.value);
-    console.log(event.target.value);
-  };
-
   return (
     <div>
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input type="text" value={value} onChange={onChange} />
-      <button onClick={Increment}>Increment</button>
-      <button onClick={Decrement}>Decrement</button>
+      <Counter />
     </div>
   );
 };
