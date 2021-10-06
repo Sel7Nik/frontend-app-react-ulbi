2067eaf8c94de8ddac128558fe9029f842199c20
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-06 19:56:09 +0300

    const [posts, setPosts] = useState  {posts.map((post) => (         <PostItem post={post} key={post.id} />       ))}

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PostItem from './components/PostItem';
 import './styles/App.css';
 const App = () => {
-  const id = 1;
-  const title = '1. Javascript';
-  const text = ' Javascript это язык программирования';
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: '1. Javascript 1',
+      text: ' Javascript это язык программирования',
+    },
+    {
+      id: 2,
+      title: '1. Javascript 2',
+      text: ' Javascript это язык программирования',
+    },
+    {
+      id: 3,
+      title: '1. Javascript 3',
+      text: ' Javascript это язык программирования',
+    },
+  ]);
+
   return (
     <div className="App">
       <h1 className="App__title" style={{ textAlign: 'center' }}>
         Список постов
       </h1>
-      <PostItem post={{ id: id, title: title, text: text }} />
-      <PostItem post={{ id: id, title: title, text: text }} />
-      <PostItem post={{ id: id, title: title, text: text }} />
-      <PostItem post={{ id: id, title: title, text: text }} />
+      {posts.map((post) => (
+        <PostItem post={post} key={post.id} />
+      ))}
     </div>
   );
 };
