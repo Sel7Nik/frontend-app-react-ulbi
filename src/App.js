6d253e3b2d0047fe2cb09d928756aba38d019a62
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-07 17:43:25 +0300

    posts.map((post, index) => (         <PostItem number={index + 1}

--- src/App.js
+++ src/App.js
@@ -8,18 +8,18 @@ const App = () => {
   const [posts, setPosts] = useState([
     {
       id: 1,
-      title: '1. Javascript 1',
-      text: ' Javascript это язык программирования',
+      title: ' Javascript 1',
+      body: ' Javascript это язык программирования',
     },
     {
       id: 2,
-      title: '2. Javascript 2',
-      text: ' Javascript это язык программирования',
+      title: 'Javascript 2',
+      body: ' Javascript это язык программирования',
     },
     {
       id: 3,
-      title: '3. Javascript 3',
-      text: ' Javascript это язык программирования',
+      title: 'Javascript 3',
+      body: ' Javascript это язык программирования',
     },
   ]);
 
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import MyButton from './UI/button/MyButton';
 
-const PostItem = ({ post }) => {
-  const { id, title, text } = post;
+const PostItem = ({ post, ...props }) => {
+  const { title, body } = post;
   return (
     <div>
       <div className="post">
         <div className="post__content">
-          <h2 className="post__id">{id}</h2>
-          <strong className="post__content_title">{title}</strong>
-          <div className="post__content_text">{text}</div>
+          <strong className="post__content_title">
+            {props.number}.{title}
+          </strong>
+          <div className="post__content_text">{body}</div>
         </div>
         <div className="post__btns">
-          {/* <button className="post__btn">Удалить</button> */}
           <MyButton>Удалить</MyButton>
         </div>
       </div>
