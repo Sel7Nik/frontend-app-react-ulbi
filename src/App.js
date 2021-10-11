5103fdf2921ec8870922cd22c9bd819deaaecf7e
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-11 18:53:09 +0300

    console.log(response.headers['x-total-count'])

--- src/App.js
+++ src/App.js
@@ -11,22 +11,22 @@ import { usePosts } from './hooks/usePosts'
 
 import './styles/App.css'
 const App = () => {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: 'aaa',
-      body: 'aaa',
-    },
-  ])
   //!--- hooks состояний
+  const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({ sort: '', query: '' })
   const [modal, setModal] = useState(false)
+  const [totalCount, setTotalCount] = useState(0)
+  const [limit, setLimit] = useState(10)
+  const [page, setPage] = useState(1)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+
   const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll()
-    setPosts(posts)
+    const response = await PostService.getAll(limit, page)
+    setPosts(response.data)
+    console.log(response.headers['x-total-count'])
+    setTotalCount(response.headers['x-total-count'])
   })
 
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
   //!--- hook состояние mount
   //!--- массив зависимостей пустой - callback отработает один раз
   useEffect(() => {
