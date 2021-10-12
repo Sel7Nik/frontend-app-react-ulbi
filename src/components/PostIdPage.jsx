08354136020b18ae2da88fedf770b99bc6434d0d
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-12 16:45:06 +0300

    useHistory, useParams fetchPostById(params.id)

--- src/components/PostIdPage.jsx
+++ src/components/PostIdPage.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import PostService from '../API/PostService'
+import { useFetching } from '../hooks/useFetching'
 const PostIdPage = () => {
   const params = useParams()
-  console.log(params)
+  const [post, setPost] = useState({})
+  const [fetchPostById, isLoading, error] = useFetching(async (id) => {
+    const response = await PostService.getById(id)
+    setPost(response.data)
+  })
+
+  useEffect(() => {
+    fetchPostById(params.id)
+  }, [])
 
   return (
     <div>
       <h1>Вы открыли страницу поста {params.id}</h1>
+      <div>
+        {post.id}.{post.title}
+      </div>
     </div>
   )
 }
