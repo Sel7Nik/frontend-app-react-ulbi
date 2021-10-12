0c4c53ec96503174a0a53f4cd88cc9e41c1c5558
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-12 15:20:42 +0300

    Redirect Error 404

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import Navbar from './components/UI/Navbar/Navbar'
 import About from './pages/About'
+import Error from './pages/Error'
 import Posts from './pages/Posts'
 import './styles/App.css'
 const App = () => {
@@ -14,6 +15,10 @@ const App = () => {
         <Route path="/posts">
           <Posts />
         </Route>
+        <Route path="/error">
+          <Error />
+        </Route>
+        <Redirect to="/error" />
       </Switch>
     </BrowserRouter>
   )
